094b15b76be597eab29b872ae7185532626ff02e
13shourya <169061666+13shourya@users.noreply.github.com>
2025-04-07 14:40:12 -0400

    PdfResponse.java

--- src/main/java/com/shourya/myfirstmavenproject/model/PdfResponse.java
+++ src/main/java/com/shourya/myfirstmavenproject/model/PdfResponse.java
@@ -1,4 +1,23 @@
 package com.shourya.myfirstmavenproject.model;
 
 public class PdfResponse {
+    private boolean success;
+    private String error;
+
+    // Getter and Setter
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public void setSuccess(boolean success) {
+        this.success = success;
+    }
+
+    public String getError() {
+        return error;
+    }
+
+    public void setError(String error) {
+        this.error = error;
+    }
 }
