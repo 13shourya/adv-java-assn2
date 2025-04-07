e033661a0c1d12f303d9b9c53a06c356fd66dfa6
13shourya <169061666+13shourya@users.noreply.github.com>
2025-04-07 14:40:47 -0400

    Main.java

--- src/main/java/com/shourya/myfirstmavenproject/Main.java
+++ src/main/java/com/shourya/myfirstmavenproject/Main.java
@@ -1,4 +1,22 @@
 package com.shourya.myfirstmavenproject;
 
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.Parent;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/com/shourya/myfirstmavenproject/view/MainView.fxml"));
+        primaryStage.setTitle("PDF Converter with pdflayer");
+        primaryStage.setScene(new Scene(root, 400, 200));
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
